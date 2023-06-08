438926206c060428530a18939fb8c799b98d3564
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-06-08 19:48:53 +0530

    Create array sum of even element.c

---
+++ Level#3/array sum of even element.c
@@ -0,0 +1,18 @@
+#include<stdio.h>    
+int main()    
+{
+    int n,sum=0;
+    printf("enter the lenght of array:");
+    scanf("%d",&n);
+    int a[n];
+    for(int i =0;i<n;i++){
+        printf("enter the %d element:",i);
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]%2==0){
+            sum+=a[i];
+        }
+    }
+    printf(" the sum is:%d",sum);
+}
